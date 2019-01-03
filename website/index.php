17bb7be6de867eb80876ebe6f95d3fde72bce632
ravirathod9595 <44313377+ravirathod9595@users.noreply.github.com>
2019-01-03 18:07:22 +0530

    Update index.php

--- website/index.php
+++ website/index.php
@@ -7,7 +7,7 @@
 	    <h2>Candidate Information Form</h2>
 	    <ul>
 		<?php
-  		    $json = file_get_contents('10.102.175.89:80');
+  		    $json = file_get_contents('http://10.102.175.89:80');
   		    $obj = json_decode($json);
 		    $products = $obj->products;
 		    foreach ($products as $product) {
